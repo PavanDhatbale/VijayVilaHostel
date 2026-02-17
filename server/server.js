42b29ed18f822b04689b8859aa66ce4aeff78890
PavanDhatbale <pavandhatbale17@gmail.com>
2026-02-17 22:14:43 +0530

    update video section

--- server/server.js
+++ server/server.js
@@ -14,8 +14,14 @@ connectDB();
 // Middleware
 // app.use(cors());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
+    origin: [
+        process.env.FRONTEND_URL,
+        'https://vijay-vila-hostel.vercel.app',
+        'http://localhost:5173'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
