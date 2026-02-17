83302eec9ef063c066829cd6145ec8d4e16f68bd
PavanDhatbale <pavandhatbale17@gmail.com>
2026-02-17 22:21:32 +0530

    final update

--- server/server.js
+++ server/server.js
@@ -13,20 +13,41 @@ connectDB();
 
 // Middleware
 // app.use(cors());
-app.use(cors({
-    origin: [
-        process.env.FRONTEND_URL,
-        'https://vijay-vila-hostel.vercel.app',
-        'http://localhost:5173'
-    ],
+// app.use(cors({
+//     origin: [
+//         process.env.FRONTEND_URL,
+//         'https://vijay-vila-hostel.vercel.app',
+//         'http://localhost:5173'
+//     ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+const cors = require("cors");
+
+const allowedOrigins = [
+  "http://localhost:5173", // local dev
+  "https://vijay-vila-hostel.vercel.app", // production frontend
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+  })
+);
 
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
